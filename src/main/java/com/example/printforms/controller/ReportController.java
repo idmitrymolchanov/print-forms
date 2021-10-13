355281e9f116d49fb031d06af1edff5163aacb1a
idmitrymolchanov <idmitrymolchanov@gmail.com>
2021-10-13 10:03:00 +0300

    key-value

--- src/main/java/com/example/printforms/controller/ReportController.java
+++ src/main/java/com/example/printforms/controller/ReportController.java
@@ -4,12 +4,10 @@ import com.example.printforms.service.reports.ReportService;
 import lombok.extern.slf4j.Slf4j;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Base64;
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -22,24 +20,26 @@ public class ReportController {
         this.service = service;
     }
 
-    @GetMapping("/generate/id/{id}/format/{format}/data/{data}")
+    @PostMapping("/generate/id/{id}/format/{format}/data/{data}")
     public byte[] getReport(@PathVariable("id") String reportId,
                             @PathVariable("format") String reportFormat,
-                            @PathVariable("data") byte[] reportData) {
+                            //                           @PathVariable("data") byte[] reportData)
+                            @RequestBody List<Item> items)
+            {
 
-        System.out.println(reportId + " " + reportFormat);
-        byte[] decoded = Base64.getDecoder().decode(reportData);
-
-        //JSONObject jsonObject = new JSONObject(new String(decoded));
-        try {
-            //String reportDataDecoded = jsonObject.toString();
-            //System.out.println(reportDataDecoded);
-            return service.getReportInBase64(reportId, reportFormat, new String(decoded));
-        }
-        catch (Exception ignored) {
-
-        }
-        log.error("error");
+//        System.out.println(reportId + " " + reportFormat);
+//        byte[] decoded = Base64.getDecoder().decode(reportData);
+//
+//        //JSONObject jsonObject = new JSONObject(new String(decoded));
+//        try {
+//            //String reportDataDecoded = jsonObject.toString();
+//            //System.out.println(reportDataDecoded);
+//            return service.getReportInBase64(reportId, reportFormat, new String(decoded));
+//        }
+//        catch (Exception ignored) {
+//
+//        }
+//        log.error("error");
         return null;
     }
 
